758cda61a500f4e094f6307cb48fc8dfe8c68f58
Alfred Huang <alfredhuang@dyn-209-2-225-69.dyn.columbia.edu>
2014-02-19 11:40:23 -0500

    Update utilities

--- Client.bat
+++ Client.bat
@@ -1,7 +1,1 @@
-@echo off
-
-javac CommObject.java
-javac Client*.java
 java Client localhost 5566
-
-@echo on
--- Server.bat
+++ Server.bat
@@ -1,7 +1,2 @@
-@echo off
-
-javac CommObject.java
-javac Server*.java
 java Server 5566
 
-@echo on
